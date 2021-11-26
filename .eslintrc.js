389b3de836d0946667cc434bf0234f267cd94226
V.Postupinsky <vyacheslav17@mail.ru>
2021-11-26 18:23:18 +0300

    init .eslint.js

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "browser": true,
+        "es2021": true,
+        "node": true,
+    },
+    "extends": ["eslint:recommended", "airbnb", "prettier"],
+    "plugins": ["prettier"],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
