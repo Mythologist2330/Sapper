51a4bddb6c217e81abfbfd5bd0062db08b88893f
V.Postupinsky <vyacheslav17@mail.ru>
2021-12-04 19:51:22 +0300

    Расширение функционала

--- src/script.js
+++ src/script.js
@@ -1,141 +1,181 @@
-const sapper = document.getElementById('sapper');
-let area = [];
+(function () {
+    let area = [];
+    let bombs = [];
 
-/**
- * Получение позиции соседних клеток на поле
- * @param {number} x координата по x
- * @param {number} y координата по y
- * @returns массив с позициями соседних клеток
- */
-function getCoordsNeighbours(x, y) {
-    return [
-        [x - 1, y - 1],
-        [x - 1, y],
-        [x - 1, y + 1],
-        [x, y - 1],
-        [x, y + 1],
-        [x + 1, y - 1],
-        [x + 1, y],
-        [x + 1, y + 1],
-    ];
-}
+    const sapper = document.getElementById('sapper');
+    const form = document.forms['start-form'];
+    form.addEventListener('submit', submitForm);
 
-/**
- * Проверка наличия бомбы
- * @param {number} x координата по x
- * @param {number} y координата по y
- * @returns число есть ли бомба в клетке
- */
-function checkBombOnField(x, y) {
-    return area[x][y].isBomb ? 1 : 0;
-}
+    /**
+     * Получает из формы размеры поля и кол-во бомб на нём
+     * @param {SubmitEvent} e submit event
+     */
+    function submitForm(e) {
+        e.preventDefault();
+        const x = e.target.children['coord-x'].value;
+        const y = e.target.children['coord-y'].value;
+        const bombs = e.target.children['bombs'].value;
 
-/**
- * Проверка соседних клеток на наличие бомб
- * @param {number} x координата по x
- * @param {number} y координата по y
- * @returns число с кол-во бомб вокруг
- */
-function checkNeighbours(x, y) {
-    let bomb = 0;
-    getCoordsNeighbours(x, y)
-        .filter(isFieldExists)
-        .forEach(elem => {
-            bomb += checkBombOnField(elem[0], elem[1]);
-        });
-    document.getElementById(`${x}` + `${y}`).style.backgroundColor = '#333';
-    document.getElementById(`${x}` + `${y}`).innerHTML = bomb ? bomb : null;
-    return bomb;
-}
+        if (area.length) {
+            clearArea();
+        }
+        createArea(x, y, bombs);
+        sapper.addEventListener('click', processingId);
+    }
 
-/**
- * Проверка кликнутой клетки на наличие бомбы или запуск проверки соседних клеток
- * @param {number} x координата по x
- * @param {number} y координата по y
- */
-function clickOnField(x, y) {
-    const value = checkNeighbours(x, y);
+    /**
+     * Возвращает начальное состояние приложения
+     */
+    function clearArea() {
+        area = [];
+        sapper.replaceChildren();
+    }
 
-    if (area[x][y].isBomb) {
-        document.getElementById(`${x}` + `${y}`).innerHTML = 'bomb';
-        console.log('GAME OVER');
-    } else if (value === 0) {
+    /**
+     * Получение позиции соседних клеток на поле
+     * @param {number} x координата по x
+     * @param {number} y координата по y
+     * @returns массив с позициями соседних клеток
+     */
+    function getCoordsNeighbours(x, y) {
+        return [
+            [x - 1, y - 1],
+            [x - 1, y],
+            [x - 1, y + 1],
+            [x, y - 1],
+            [x, y + 1],
+            [x + 1, y - 1],
+            [x + 1, y],
+            [x + 1, y + 1],
+        ];
+    }
+
+    /**
+     * Проверка наличия бомбы
+     * @param {number} x координата по x
+     * @param {number} y координата по y
+     * @returns число есть ли бомба в клетке
+     */
+    function checkBombOnField(x, y) {
+        return area[x][y].isBomb ? 1 : 0;
+    }
+
+    /**
+     * Проверка соседних клеток на наличие бомб
+     * @param {number} x координата по x
+     * @param {number} y координата по y
+     * @returns число с кол-во бомб вокруг
+     */
+    function getAmountBombsAround(x, y) {
+        let bomb = 0;
         getCoordsNeighbours(x, y)
             .filter(isFieldExists)
-            .forEach(elem => {
-                let val = checkNeighbours(elem[0], elem[1]);
-                if (val === 0 && area[elem[0]][elem[1]].isChecked === false) {
-                    area[elem[0]][elem[1]].isChecked = true;
-                    clickOnField(elem[0], elem[1]);
-                }
-            });
+            .forEach(([x, y]) => (bomb += checkBombOnField(x, y)));
+        document.getElementById(x + '-' + y).style.backgroundColor = '#333';
+        document.getElementById(x + '-' + y).innerHTML = bomb ? bomb : null;
+        return bomb;
     }
-}
 
-/**
- * Проверяет есть ли указаная клетка на игровом поле
- * @param {[number, number]} field координаты по осям X и Y
- * @returns {boolean} true или false
- */
-function isFieldExists(field) {
-    const [x, y] = field;
-    return x >= 0 && y >= 0 && x < area.length && y < area[0].length;
-}
-
-/**
- * Обработка ids и запуск функции с проверками
- * @param {*} event
- */
-function processingId(event) {
-    idX = +event.path[1].id.split('')[1];
-    idY = +event.path[0].id.split('')[1];
-    clickOnField(idX, idY);
-}
-
-/**
- * Отрисовка поля на frontend
- * @param {number} x координата по x
- * @param {number} y координата по y
- * @param {number} bombs кол-во бомб на поле
- */
-function createArea(x, y, bombs) {
-    for (let i = 0; i < x; i++) {
-        const lineX = [];
-        const elemX = document.createElement('div');
-        sapper.appendChild(elemX).setAttribute('id', `x${i}`);
-
-        for (let j = 0; j < y; j++) {
-            lineX.push({
-                name: j,
-                isBomb: false,
-                isChecked: false,
-            });
-            const elemY = document.createElement('div');
-            elemX.appendChild(elemY).setAttribute('id', `${i}${j}`);
-        }
-        area.push(lineX);
-    }
-    area = renderBombs(bombs);
-}
-
-/**
- * Создание бомб на поле
- * @param {number} bombsAmount кол-во бомб
- * @returns поле с бомбами
- */
-function renderBombs(bombsAmount) {
-    for (let i = 0; i < bombsAmount; i++) {
-        const x = Math.floor(Math.random() * area.length);
-        const y = Math.floor(Math.random() * area[0].length);
-
-        if (!area[x][y].isBomb) {
-            area[x][y].isBomb = true;
-        } else {
-            i--;
+    /**
+     * Проверка кликнутой клетки на наличие бомбы или запуск проверки соседних клеток
+     * @param {number} x координата по x
+     * @param {number} y координата по y
+     */
+    function clickOnField(x, y) {
+        const amountBombs = getAmountBombsAround(x, y);
+        if (area[x][y].isBomb) {
+            gameOver();
+        } else if (!amountBombs) {
+            getCoordsNeighbours(x, y).filter(isFieldExists).forEach(checkField);
         }
     }
-    return area;
-}
 
-createArea(10, 10, 10);
-sapper.addEventListener('click', processingId);
+    /**
+     * Game Over :)
+     */
+    function gameOver() {
+        sapper.removeEventListener('click', processingId);
+        bombs.forEach(([x, y]) => {
+            document.getElementById(x + '-' + y).style.backgroundColor = '#f00';
+            document.getElementById(x + '-' + y).innerHTML = '!';
+        });
+    }
+
+    /**
+     * Проверяет клетку
+     * @param {[number, number]} field координаты по осям X и Y
+     */
+    function checkField(field) {
+        const [x, y] = field;
+        let val = getAmountBombsAround(x, y);
+        if (!val && !area[x][y].isChecked) {
+            area[x][y].isChecked = true;
+            clickOnField(x, y);
+        }
+    }
+
+    /**
+     * Проверяет есть ли указаная клетка на игровом поле
+     * @param {[number, number]} field координаты по осям X и Y
+     * @returns {boolean} true или false
+     */
+    function isFieldExists(field) {
+        const [x, y] = field;
+        return x >= 0 && y >= 0 && x < area.length && y < area[0].length;
+    }
+
+    /**
+     * Обработка ids и запуск функции с проверками
+     * @param {MouseEvent} event
+     */
+    function processingId(event) {
+        const [idX, idY] = event.path[0].id.split('-');
+        clickOnField(parseInt(idX), parseInt(idY));
+    }
+
+    /**
+     * Отрисовка поля на frontend
+     * @param {number} x координата по x
+     * @param {number} y координата по y
+     * @param {number} bombs кол-во бомб на поле
+     */
+    function createArea(x, y, bombs) {
+        for (let i = 0; i < x; i++) {
+            const lineX = [];
+            const elemX = document.createElement('div');
+            sapper.appendChild(elemX).setAttribute('id', `x${i}`);
+
+            for (let j = 0; j < y; j++) {
+                lineX.push({
+                    name: j,
+                    isBomb: false,
+                    isChecked: false,
+                });
+                const elemY = document.createElement('div');
+                elemX.appendChild(elemY).setAttribute('id', `${i}-${j}`);
+            }
+            area.push(lineX);
+        }
+        renderBombs(bombs);
+    }
+
+    /**
+     * Создание бомб на поле
+     * @param {number} bombsAmount кол-во бомб
+     * @returns поле с бомбами
+     */
+    function renderBombs(bombsAmount) {
+        for (let i = 0; i < bombsAmount; i++) {
+            const x = Math.floor(Math.random() * area.length);
+            const y = Math.floor(Math.random() * area[0].length);
+            bombs.push([x, y]);
+
+            if (!area[x][y].isBomb) {
+                area[x][y].isBomb = true;
+            } else {
+                i--;
+            }
+        }
+        return area;
+    }
+})();
